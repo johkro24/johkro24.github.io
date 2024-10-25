1cab8e41232221d20329fe47ab729ccd1d72ea02
CozyKaspi <johkro24@innlandetfylke.no>
2024-10-25 21:45:37 +0200

    Deploy to GitHub Pages using docs folder

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,10 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export',               // Enables static export
-  distDir: 'docs',                // Specify docs as the output directory for GitHub Pages
-  basePath: '/johkro24.github.io',  // Set this to the repository name
-  assetPrefix: '/johkro24.github.io/' // Same as above for asset paths
+  output: 'export', // Enables static export
+  basePath: '/johkro24.github.io', // For GitHub Pages
+  assetPrefix: '/johkro24.github.io/', // For GitHub Pages
 };
 
 export default nextConfig;
+
